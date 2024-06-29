66fa2444afc8c8464be6ea047d1fcae2d2e39e1c
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2024-06-30 01:49:25 +0800

    Set-up public folder for static CSS and JS files

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000;
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 // Define a route for the root URL
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
