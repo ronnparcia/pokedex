99b1e481178cbc1476e6878dd7fbaaf69abdb711
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2024-07-02 12:20:26 +0800

    Generate card for pokemon on Load More

--- public/js/index.js
+++ public/js/index.js
@@ -3,18 +3,59 @@ $(document).ready(function() {
         // Load more Pokemon
         $.get('/load-more', function(data) {
             data.forEach(pokemon => {
-                // Make li element and store in variable
-                const li = $(`<li>${pokemon.id} - ${pokemon.name}</li>`);
+                // Generate the Pokemon card
+                const pokemonCard = generatePokemonCard(pokemon);
                 
-                // Append img to li
-                if (pokemon.imageUrl) {
-                    const img = $(`<img src="${pokemon.imageUrl}" alt="${pokemon.name}" />`);
-                    li.append(img);
-                }
+                // Append the card to the container
+                $('#pokemon-grid').append(pokemonCard);
 
-                // Append li to ul
-                $('#pokemon-list').append(li);
+                // // Make li element and store in variable
+                // const li = $(`<li>${pokemon.id} - ${pokemon.name}</li>`);
+                
+                // // Append img to li
+                // if (pokemon.imageUrl) {
+                //     const img = $(`<img src="${pokemon.imageUrl}" alt="${pokemon.name}" />`);
+                //     li.append(img);
+                // }
+
+                // // Append li to ul
+                // $('#pokemon-list').append(li);
             });
         });
     });
+
+    function generatePokemonCard(pokemon) {
+        // Make card element and store in variable
+        const card = $(`
+            <div class="col">
+                <div class="card">
+                    <div class="row g-0">
+                    </div>
+                </div>
+            </div>
+        `);
+    
+        // Image wrapper
+        const imgWrapper = $('<div class="card-img-wrapper col-4"></div>');
+        if (pokemon.imageUrl) {
+            const img = $(`<img src="${pokemon.imageUrl}" alt="${pokemon.name}">`);
+            imgWrapper.append(img);
+        }
+    
+        // Card body
+        const cardBody = $(`
+            <div class="col-8">
+                <div class="card-body">
+                    <h5 class="card-title">${pokemon.name}</h5>
+                    <p class="card-text"><small class="text-body-secondary">ID No. ${pokemon.id}</small></p>
+                </div>
+            </div>
+        `);
+    
+        // Append image wrapper and card body to the card
+        card.find('.row').append(imgWrapper).append(cardBody);
+    
+        return card;
+    }
+            
 });
